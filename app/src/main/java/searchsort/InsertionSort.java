6439f4d70eb78680632d220ddca1797ae55fcbfa
Nicholas-lh <350373023@gapps.yrdsb.ca>
2024-12-09 10:50:06 -0500

    working on insertion sort

--- app/src/main/java/searchsort/InsertionSort.java
+++ app/src/main/java/searchsort/InsertionSort.java
@@ -4,7 +4,31 @@ public class InsertionSort {
     /**
      * Sort the array arr using insertion sort algorithm.
      */
-    public static void sort(int[] arr) {
+    public static void sort(int[] arr) 
+    {
+        int N = arr.length; 
+        for(int i = 1; i < N; i++)
+        {
+            int j = i - 1;
+            for(int j = i; j > 0; j--)
+            {
+                if(arr[j-1] > arr[j])
+                {
+                    exch(arr, j, j - 1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void exch(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
 
     }
 }
--- app/src/main/java/searchsort/SelectionSort.java
+++ app/src/main/java/searchsort/SelectionSort.java
@@ -3,8 +3,31 @@ package searchsort;
 public class SelectionSort {
     /**
      * Sort the array arr using selection sort algorithm.
+     * 
      */
-    public static void sort(int[] arr) {
-
+    public static void sort(int[] arr) 
+    {
+        //insertion sort and selection sort is the same (I believe)
+        int N = arr.length;
+        for(int i = 0 i < N - 1; i++)
+        {
+            int minIndex = i;
+            for(int j = i + 1; j < N; j++)
+            {
+                if(arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+            exch(arr, i, minIndex);
+        }
     }
+
+    public static void exch(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 }
