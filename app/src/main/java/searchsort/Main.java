2efbbcd87dcd668664d46a563b58ce12cb681645
Nicholas-lh <350373023@gapps.yrdsb.ca>
2024-12-09 22:18:28 -0500

    util

--- app/src/main/java/searchsort/Main.java
+++ app/src/main/java/searchsort/Main.java
@@ -7,17 +7,43 @@ public class Main {
     public static void main(String[] args) {
         // Test Util.exch
         int[] arr1 = { 1, 2, 3, 4, 5 };
-        Util.exch(arr1, 0, 1);
-        for (int i = 0; i < arr1.length; i++) {
-            System.out.print(arr1[i] + " ");
+        for (int num : arr1) 
+        {
+            System.out.print(num + "");
         }
+        System.out.println();
+
+        Util.exch(arr1, 0,1);
+        for(int num : arr1)
+        {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
 
         // Test Util.shuffle
-        int[] arr = { 1, 2, 3, 4, 5 };
-        Util.shuffle(arr, 12345L); // 12345L converts the number to a long
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        int[] arr2 = { 1, 2, 3, 4, 5 };
+        for (int num : arr2)
+        {
+            System.out.print(num + " ");
         }
+        System.out.println();
+
+        Util.shuffle(arr2, 12345L); // 12345L converts the number to a long
+        for(int num : arr2)
+        {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        System.out.print("Generated random array: ");
+        int[] randomArray = Util.generateRandomArray(5, 12345L);
+        for (int num : randomArray) System.out.print(num + " ");
+        System.out.println();
+
+        // Test Util.isSorted
+        System.out.println(Util.isSorted(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(Util.isSorted(arr2));
 
         //Test MergeSort
         int[] mergeArr = { 1, 2, 3, 4, 5 };
