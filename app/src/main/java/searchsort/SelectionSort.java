685ddd77a7b8bc7a4768f8e10b0dec86ef0aa6ed
Nicholas-lh <350373023@gapps.yrdsb.ca>
2024-12-10 01:03:20 -0500

    final selection sort

--- app/src/main/java/searchsort/SelectionSort.java
+++ app/src/main/java/searchsort/SelectionSort.java
@@ -4,7 +4,24 @@ public class SelectionSort {
     /**
      * Sort the array arr using selection sort algorithm.
      */
-    public static void sort(int[] arr) {
+    public static void sort(int[] arr) 
+    {
+        int n = arr.length;
 
+        for(int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+            for(int j = i + 1; j < n; j++)
+            {
+                if(arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i]; 
+            arr[i] = temp;
+        }
     }
 }
